86928e74853e56ea44dedc9d29d23de17e2b2459
Juan Fox <foxj0875@gmail.com>
2018-03-29 15:24:47 -0400

    products table is populated with products.csv

--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE products (
   item_id INTEGER(11) NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(50) NOT NULL,
   department_name VARCHAR(50) NOT NULL,
-  price INTEGER(10) NOT NULL,
+  price DECIMAL(13,2) NOT NULL,
   stock_quantity VARCHAR(50),
   PRIMARY KEY (item_id)
 );
