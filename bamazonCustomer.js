23740d1733684c5a248e99e4c9394565008864ec
Juan Fox <foxj0875@gmail.com>
2018-03-30 00:36:33 -0400

    inquirer setup to take customer requests

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -20,6 +21,34 @@ function showProducts() {
     'SELECT * FROM products',
     function(err, data) {
       console.log(data);
+      userProductRequest();
     }
   );
+}
+
+function userProductRequest() {
+  inquirer.prompt([
+    {
+      message: 'Please input the id of the product you wish to purchase',
+      name: 'product_id'
+    },
+    {
+      message: 'Please input the quantity that you wish to purchase',
+      name: 'quantity'
+    }
+  ]).then(function(answers) {
+    console.log(answers);
+    connection.query(
+      'SELECT * FROM products WHERE item_id = ' + answers.product_id,
+      function(err, data) {
+        var product = data[0];
+        console.log(product.stock_quantity);
+        // if (answers.quantity > product.stock_quantity) {
+        //   console.log('Insufficient Quantity');
+        // } else {
+        //   console.log('Here you go!');
+        // }
+      }
+    );
+  });
 }
